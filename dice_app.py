b3a42718151ce9055a7317e253a300b51a88bb9b
doctormordi <doctormordi@users.noreply.github.com>
2024-07-02 12:02:34 +0200

    Added Waitress, fixed Bad Gateway issue, updated requirements.txt with latest versions

--- dice_app.py
+++ dice_app.py
@@ -1,6 +1,7 @@
 # dice_app.py
 from flask import Flask, request, jsonify
 import random
+from waitress import serve
 
 app = Flask(__name__)
 
@@ -24,4 +25,4 @@ def roll_dice():
         return jsonify({"error": "Invalid dice type!"}), 400
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    serve(app, host='0.0.0.0', port=5000)
